12290101e7fe89b4c16399b21e49c5eca406c73a
Grunde H. Wesenberg <superheltenroy@gmail.com>
2017-08-27 23:00:55 +0200

    Made transformation for Character

--- src/cards/cardsTemplate.js
+++ src/cards/cardsTemplate.js
@@ -1,6 +1,6 @@
 export default [
 	{"atomicNumber":1,"symbol":"H","name":"Hydrogen","atomicMass":"1.00794(4)","cpkHexColor":"FFFFFF","electronicConfiguration":"1s1","electronegativity":2.2,"atomicRadius":37,"ionRadius":"","vanDelWaalsRadius":120,"ionizationEnergy":1312,"electronAffinity":-73,"oxidationStates":"-1, 1","standardState":"gas","bondingType":"diatomic","meltingPoint":14,"boilingPoint":20,"density":0.0000899,"groupBlock":"nonmetal","yearDiscovered":1766},
 	{"placeholder":0},
-	{"cardSeries":"Base Set","cardNumber":0,"cardName":"Delivery Owl","rarity":"Foil","cardType":"Creature","cost":"NA","kind":"Colorless","text":"When you play this card shit happens"}
+	{"set":"Base Set","number":0,"name":"Delivery Owl","rarity":"Foil","type":"Creature","cost":"NA","kind":"Colorless","text":"When you play this card shit happens"}
 ]
 
--- src/cards/translateCardsToJson.js
+++ src/cards/translateCardsToJson.js
@@ -2,35 +2,67 @@ fs = require('fs');
 
 var cardList = [];
 var fileString = fs.readFileSync('base_raw', 'utf8');
+var set = 'Base Set';
 
 for (let cardToken of fileString.split("\nnewcard\n")){
-	var cardLines = cardToken.split('\n');
+	let cardLines = cardToken.split('\n');
 	let card;
-	if (cardLines[1].search('Creature')){
+	console.log(cardLines[1]);
+	if (cardLines[1].search('Creature')>-1){
 		card = createCreature(cardLines);
-	} else if (cardLines[1].search('Character')){
+	} else if (cardLines[1].search('Character')>-1){
 		card = createCharacter(cardLines);
-	} else if (cardLines[1].search('Lesson')){
+	} else if (cardLines[1].search('Lesson')>-1){
 		card = createLesson(cardLines);
-	} else if (cardLines[1].search('Adventure')){
+	} else if (cardLines[1].search('Adventure')>-1){
 		card = createAdventure(cardLines);
-	} else if (cardLines[1].search('Spell')){
+	} else if (cardLines[1].search('Spell')>-1){
 		card = createSpell(cardLines);
-	} else if (cardLines[1].search('Location')){
+	} else if (cardLines[1].search('Location')>-1){
 		card = createLocation(cardLines);
-	} else if (cardLines[1].search('Match')){
+	} else if (cardLines[1].search('Match')>-1){
 		card = createMatch(cardLines);
-	} else if (cardLines[1].search('Item')){
+	} else if (cardLines[1].search('Item')>-1){
 		card = createItem(cardLines);
 	}
 	cardList.push(card);
+	console.log(card);
 }
 
-function createCharacter(cardLines){}
+function createCharacter(cardLines){
+	let card = {};
+	card.set = set;
+	card.number = cardLines[0].split(' ')[0];
+	card.name = cardLines[0].split(' ')[1];
+	card.rarity = cardLines[1].split(' ')[0];
+	card.type = cardLines[1].split(' ')[1].split('/')[0];
+	card.descriptors = [];
+	card.descriptors.push(cardLines[1].split(' ')[1].split('/')[1]);
+	if(card.descriptors[0]){
+		card.descriptors = card.descriptors.concat(cardLines[2].split('/'));
+	}
+	else{ 
+		card.descriptors = (cardLines[2].split('/'));
+	}
+	let textIndex = 3;
+	if (cardLines[3].search('Provides ')>-1){
+		card.power = cardLines[3].split(' ')[1];
+		textIndex = 4;
+	}
+	card.text = ''
+	for (let i = textIndex; i<cardLines.length; i++){
+		card.text += cardLines[i];
+	}
+	return card;
+}
 
-function createAdventure(cardLines){}
+function createAdventure(cardLines){
+	console.log('adventure')
+}
 
-function createCreature(cardLines){}
+function createCreature(cardLines){
+	console.log('creature');
+}
 
 function createItem(cardLines){}
 
